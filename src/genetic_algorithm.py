0088195911173019fbb6ac8597ceb210a3dab987
infiniator <infiniator@gmail.com>
2017-11-28 12:55:22 +0530

    Added crossover and mutation functions, although the crossover function isn't complete

--- src/genetic_algorithm.py
+++ src/genetic_algorithm.py
@@ -1,5 +1,6 @@
 from random import randrange
 from src.chromosome import Chromosome
+from src.binary_search import binarySearch
 
 population = []  # a list of chromosomes
 
@@ -8,9 +9,12 @@ def initialisation():
     # a temporary chromosome to be manipulated before adding to the population
     tempChromosome = Chromosome()
     for i in range(0, 100000):
-        for j in range(0, 512):
-            k = randrange(0, 16)
+        for j in range(0, tempChromosome.numTasks):
+            k = randrange(0, tempChromosome.numProcs)
             tempChromosome.schedule[k].append(j)
+        for j in range(0, tempChromosome.numProcs):
+            tempChromosome.schedule[j].sort()
+        # tempChromosome.schedule.sort(key=tempChromosome.schedule.itemgetter(0))
         tempChromosome.calculateFitness()
         population.append(tempChromosome)
         tempChromosome = Chromosome()
@@ -18,3 +22,28 @@ def initialisation():
 
 def selection():
     print("he")
+
+
+def crossover(a, b):
+    randProc = randrange(0, a.numProcs)
+    randTask = randrange(0, a.numTasks)
+    indexA = -1
+    indexB = -1
+    for i in range(0, b.numProcs):
+        indexB = binarySearch(b[i], randTask)
+        indexA = binarySearch(a[i], randTask)
+
+
+def mutation(a):
+    removeFrom = -1
+    addTo = -1
+    while removeFrom == addTo and len(a.schedule[removeFrom]) > 0:
+        removeFrom = randrange(0, a.numProcs)
+        addTo = randrange(0, a.numProcs)
+    randTask = randrange(0, len(a.schedule[removeFrom]))
+    a.schedule[addTo].insert(
+        binarySearch(a.schedule[addTo], a.schedule[removeFrom][randTask]),
+        a
+    )
+    del a.schedule[removeFrom][randTask]
+    return a
